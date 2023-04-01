38a4820a18457f947327c07293a92335a2cad4a9
Santhoshgow <129221779+Santhoshgow@users.noreply.github.com>
2023-04-01 18:15:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,8 +31,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Vk:123@autofilterbot.n4grvhp.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "autofilter")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sk:123@santhosh.kzzhdc7.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "santhosh")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
